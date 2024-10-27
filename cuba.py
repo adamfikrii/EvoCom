df7aa4f76bd2e63f3cc77769a7c5ec2a7e6a9424
adamfikrii <95835278+adamfikrii@users.noreply.github.com>
2024-10-27 11:27:07 +0800

    Update cuba.py

--- cuba.py
+++ cuba.py
@@ -15,7 +15,7 @@ MUT_RATE = 0.2
 
 #TARGET: Our goal.
 #TARGET = 'Adam'
-TARGET = st.text_input("Enter your name")
+TARGET = st.text_input("Enter your name","Adam")
 
 #GENES: Options from which our population would be created.
 GENES = ' abcdefghijklmnopqrstuvwxyzQWERTYUIOPLKJHGFDSAZXCVBNM'
