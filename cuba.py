61ff8a40a0d51467627e822023f51263486648ac
adamfikrii <95835278+adamfikrii@users.noreply.github.com>
2024-10-28 15:31:52 +0800

    Update cuba.py

--- cuba.py
+++ cuba.py
@@ -12,7 +12,7 @@ POP_SIZE = 500
 
 #MUT_RATE: Rate at which our string will be changed.
 #MUT_RATE = 0.2
-MUT_RATE = st.number_input("Enter your mutation rate", 0.2)
+MUT_RATE = st.number_input("Enter your mutation rate")
 
 #TARGET: Our goal.
 #TARGET = 'Adam'
