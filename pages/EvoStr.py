f3e2cf7403012fa97f4ebf74b0d774143b3d287d
adamfikrii <95835278+adamfikrii@users.noreply.github.com>
2024-11-12 10:55:59 +0800

    Update EvoStr.py

--- pages/EvoStr.py
+++ pages/EvoStr.py
@@ -1,31 +1,34 @@
-# ackley multimodal function
-from numpy import arange
-from numpy import exp
-from numpy import sqrt
-from numpy import cos
-from numpy import e
-from numpy import pi
-from numpy import meshgrid
-from matplotlib import pyplot
+import numpy as np
+import streamlit as st
+from numpy import exp, sqrt, cos, e, pi
+from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# objective function
+# Ackley function
 def objective(x, y):
-	return -20.0 * exp(-0.2 * sqrt(0.5 * (x**2 + y**2))) - exp(0.5 * (cos(2 * pi * x) + cos(2 * pi * y))) + e + 20
+    return -20.0 * exp(-0.2 * sqrt(0.5 * (x**2 + y**2))) - exp(0.5 * (cos(2 * pi * x) + cos(2 * pi * y))) + e + 20
 
-# define range for input
-r_min, r_max = -15.0, 15.0
-# sample input range uniformly at 0.1 increments
-xaxis = arange(r_min, r_max, 0.1)
-yaxis = arange(r_min, r_max, 0.1)
-# create a mesh from the axis
-x, y = meshgrid(xaxis, yaxis)
-# compute targets
+# Streamlit interface
+st.title("Ackley Function Visualization")
+
+# Input range sliders
+r_min = st.slider("Select minimum range for x and y", -30.0, 0.0, -15.0)
+r_max = st.slider("Select maximum range for x and y", 0.0, 30.0, 15.0)
+
+# Sample input range uniformly at 0.1 increments
+xaxis = np.arange(r_min, r_max, 0.1)
+yaxis = np.arange(r_min, r_max, 0.1)
+
+# Create a mesh from the axis
+x, y = np.meshgrid(xaxis, yaxis)
+
+# Compute Ackley function values
 results = objective(x, y)
-# create a surface plot with the jet color scheme
-figure = pyplot.figure()
-# Instead of using figure.gca(), use pyplot.axes() with projection='3d' to create a 3D axes object
-axis = pyplot.axes(projection='3d') # Changed line to create 3D axes
-axis.plot_surface(x, y, results, cmap='jet')
-# show the plot
-st.pyplot(figure)
+
+# Plotting
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(x, y, results, cmap='jet')
+
+# Display the plot in Streamlit
+st.pyplot(fig)
